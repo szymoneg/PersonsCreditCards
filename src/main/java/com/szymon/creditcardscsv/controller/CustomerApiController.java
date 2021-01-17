6605f7b989cf75a433072427838e695a08b503b9
Szymon <szymon.zsp5@gmail.com>
2021-01-17 18:13:04 +0100

    +all service impl, +few controllers

--- src/main/java/com/szymon/creditcardscsv/controller/CustomerApiController.java
+++ src/main/java/com/szymon/creditcardscsv/controller/CustomerApiController.java
@@ -1,19 +1,24 @@
 package com.szymon.creditcardscsv.controller;
 
 import com.szymon.creditcardscsv.domain.dao.Person;
+import com.szymon.creditcardscsv.domain.dto.PersonEditRequest;
 import com.szymon.creditcardscsv.domain.dto.PersonRequest;
 import com.szymon.creditcardscsv.service.PersonService;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
+@Controller
+@ResponseBody
 public class CustomerApiController {
     final PersonService personService;
+
 
     public CustomerApiController(PersonService personService) {
         this.personService = personService;
@@ -33,6 +38,17 @@ public class CustomerApiController {
     public ResponseEntity<?> addNewPerson(@RequestBody PersonRequest personRequest){
         personService.create(personRequest);
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping("/persons/edit/{id}")
+    public ResponseEntity<?> editPersonSubmit(@RequestBody PersonEditRequest personEditRequest, @PathVariable("id") int id){
+        Optional<Person> optionalPerson = personService.getById(id);
+        if (optionalPerson.isPresent()){
+            personService.update(id,personEditRequest);
+            return new ResponseEntity<>("Zmieniono", HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>("Nie zmieniono", HttpStatus.BAD_REQUEST);
+        }
     }
 
     //TODO next method
--- src/main/java/com/szymon/creditcardscsv/service/impl/PersonServiceImpl.java
+++ src/main/java/com/szymon/creditcardscsv/service/impl/PersonServiceImpl.java
@@ -7,43 +7,118 @@ import com.szymon.creditcardscsv.domain.dto.StatisticsResponse;
 import com.szymon.creditcardscsv.domain.dto.enums.PropertyType;
 import com.szymon.creditcardscsv.domain.dto.enums.StatisticsType;
 import com.szymon.creditcardscsv.service.PersonService;
+import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
+@Service
 public class PersonServiceImpl implements PersonService {
+    ArrayList<Person> personArrayList;
+
+    public PersonServiceImpl(){
+        this.personArrayList = new ArrayList<>();
+    }
+
+
     @Override
     public Optional<Person> getById(int id) {
-        return Optional.empty();
+        return personArrayList.stream().filter(person -> person.getId() == id).findFirst();
     }
 
     @Override
     public List<Person> getAll() {
-        return null;
+        return personArrayList;
     }
 
     @Override
     public List<Person> getByProperty(PropertyType propertyType, String query) {
-        return null;
+        List<Person> personList;
+        switch (propertyType){
+            case id:
+                personList = personArrayList.stream().filter(person -> person.getId() == Integer.parseInt(query)).collect(Collectors.toList());
+                break;
+            case firstName:
+                personList = personArrayList.stream().filter(person -> person.getFirstName().startsWith(query)).collect(Collectors.toList());
+                break;
+            case lastName:
+                personList = personArrayList.stream().filter(person -> person.getLastName().startsWith(query)).collect(Collectors.toList());
+                break;
+            case email:
+                personList = personArrayList.stream().filter(person -> person.getEmail().startsWith(query)).collect(Collectors.toList());
+                break;
+            case gender:
+                personList = personArrayList.stream().filter(person -> person.getGender().startsWith(query)).collect(Collectors.toList());
+                break;
+            case creditCardType:
+                personList = personArrayList.stream().filter(person -> person.getCreditCardType().startsWith(query)).collect(Collectors.toList());
+                break;
+            case creditCardNumber:
+                personList = personArrayList.stream().filter(person -> person.getCreditCardNumber().startsWith(query)).collect(Collectors.toList());
+                break;
+            default:
+                return Collections.emptyList();
+        }
+        return personList;
     }
 
     @Override
     public Person create(PersonRequest personRequest) {
-        return null;
+        int index = personArrayList.isEmpty() ? 1 : (personArrayList.stream().mapToInt(Person::getId).max().getAsInt()) + 1;
+        Person newPerson = Person.builder()
+                .id(index)
+                .firstName(personRequest.getFirstName())
+                .lastName(personRequest.getLastName())
+                .email(personRequest.getEmail())
+                .gender(personRequest.getGender())
+                .creditCardType(personRequest.getCreditCardType())
+                .creditCardNumber(personRequest.getCreditCardNumber())
+                .build();
+        personArrayList.add(newPerson);
+        return newPerson;
     }
 
     @Override
     public Optional<Person> update(int id, PersonEditRequest personEditRequest) {
-        return Optional.empty();
+        return personArrayList.stream().filter(person -> person.getId() == id).peek(person -> {
+            person.setFirstName(personEditRequest.getFirstName());
+            person.setLastName(personEditRequest.getLastName());
+            person.setGender(personEditRequest.getGender());
+            person.setEmail(personEditRequest.getEmail());
+            person.setCreditCardType(personEditRequest.getCreditCardType());
+            person.setCreditCardNumber(personEditRequest.getCreditCardNumber());
+        }).findFirst();
     }
 
     @Override
     public void delete(int id) {
-
+        personArrayList.stream().filter(person -> person.getId() == id).findFirst().ifPresent(personArrayList::remove);
     }
 
     @Override
     public List<StatisticsResponse> getStatistics(StatisticsType statisticsType) {
-        return null;
+        List<String> statisticsList;
+        switch (statisticsType){
+            case gender:
+                statisticsList = personArrayList.stream().map(Person::getGender).collect(Collectors.toList());
+                break;
+            case creditCard:
+                statisticsList = personArrayList.stream().map(Person::getCreditCardType).collect(Collectors.toList());
+                break;
+            default:
+                statisticsList = Collections.emptyList();
+        }
+
+        Set<String> uniqueStatisticsList = new HashSet<>(statisticsList);
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String key : uniqueStatisticsList){
+            map.put(key, Collections.frequency(statisticsList, key));
+        }
+
+        ArrayList<StatisticsResponse> statsList = new ArrayList<>();
+        map.forEach((key, value) -> statsList.add(new StatisticsResponse(key,value)));
+
+        return statsList;
     }
 }
